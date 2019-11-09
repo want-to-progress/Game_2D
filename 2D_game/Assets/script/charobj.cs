fc55c78c9f57c66cf8e15e33d08a0eb48580a053
YUN <32327398+want-to-progress@users.noreply.github.com>
2019-11-09 21:01:13 +0800

    2019/11/9
    add script

--- 2D_game/Assets/script/charobj.cs
+++ 2D_game/Assets/script/charobj.cs
@@ -5,7 +5,34 @@ using UnityEngine;
 public class charobj : MonoBehaviour
 {
     [Header("跳躍高度"),Range(0,1000)]
-    public int  height = 0;
+    public int  height = 200;
     [Header("是否活著")]
     public bool alive = true; 
+
+    public Transform character;
+
+    private void Start() {
+        print(character.position);
+    }
+
+    private void Update() {
+       Jump();
+    }
+
+    ///<summary>
+    ///角色跳躍
+    ///</summary>
+    private void Jump(){
+         if (Input.anyKeyDown){
+            Debug.Log("A key or mouse click has been detected");
+            character.Translate(0,height*Time.deltaTime,0);
+        }
+    }
+
+    ///<summary>
+    ///角色死亡
+    ///</summary>
+    private void Dead(){
+         
+    }
 }
--- 2D_game/Assets/script/ground.cs
+++ 2D_game/Assets/script/ground.cs
@@ -4,6 +4,47 @@ using UnityEngine;
 
 public class ground : MonoBehaviour
 {
-  public int speed = 10; //moving ground speed
-  
+  public float speed = 10; //moving ground speed
+  private float init_pos_x;
+  //want to control the position
+  public Transform floor;
+  private void Start() {
+    //print(floor.position);
+    init_pos_x = floor.position.x;
+  }
+
+  private void Update() {
+    Move();
+    check_ground_region();
+  }
+
+  /// <summary>
+  /// 在快捷選單上加上註解
+  /// 確認地板範圍
+  /// </summary>
+  private void check_ground_region(){
+    if(floor.position.x < init_pos_x - 100) 
+      floor.Translate(100*Time.deltaTime,0,0); //if run out of ground
+  }
+
+  /// <summary>
+  /// 地板移動
+  /// </summary>
+  private void Move(){
+    //we can not do this
+    //floor.position.x++;
+    
+    //transformObject.Translate(float x,float y, float z);
+    //move the floor to the left
+    floor.Translate(-speed *Time.deltaTime,0,0);
+    //it means x = x - speed 
+    //Time.deltaTime --> 每幀的時間
+    /*
+      Normalize
+      Time.deltaTime = 1/frequence(Hz);
+      user A  60Hz * speed * Time.deltaTime = speed
+      user B  10Hz * speed * Time.deltaTime = speed
+    */
+     //print(floor.position);
+  }
 }
