c790f223da87b918b5328ec8cdaba2e04de16c92
YUN <32327398+want-to-progress@users.noreply.github.com>
2019-11-16 17:01:52 +0800

    2019/11/16
    noon

---
+++ 2D_game/Assets/script/LearnAPI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnAPI : MonoBehaviour
+{
+    /*
+        structure of a Project
+        1.class
+        2.field & properties
+        3.Method
+     */
+    //欄位
+    public Transform TransformA;
+    public Camera C;
+    public AudioSource Music;
+    private void Start() {
+        #region static-member
+         //use a static member(field & properties) in a class
+         print("隨機:" + Random.value);
+
+         print("PI:" + Mathf.PI);
+
+         //use a static member(method) in a class
+         print("隨機範圍:" + Random.Range(0,100));
+         print(Mathf.Abs(-10));
+        #endregion
+
+         //use a non-static member in a class
+         //需要先給予欄位存放實體物件
+        print("物件A座標:" + TransformA.position); //unity 內部拉取欄位
+       
+        TransformA.position =  new Vector3(-2,0,0);//character postiion
+        
+        print("相機深度:" + C.depth);
+
+        Music.mute = true;
+    }
+
+    private void Update() {
+        TransformA.Rotate(10,0,0);
+    }
+}
---
+++ 2D_game/Assets/script/LearnIF.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnIF : MonoBehaviour
+{
+    public bool test;
+    public string prop;
+
+    [Header("血量"),Range(0,100)]
+    public int HP = 100;
+
+    //60fps
+    private void Update(){
+        if (test)
+        {
+            print("打開開關~");
+        }
+        else{
+            print("關閉開關~");
+        }
+
+        /*
+        #region prop
+        if (prop == "紅色藥水")
+        {
+            print("補血");
+        }
+        else if (prop == "藍色藥水")
+        {
+            print("補魔力");
+        }
+        else if (prop == "黃色藥水")
+        {
+            print("補體力");
+        }
+        else{
+            print("沒事情發生~~");
+        }
+        #endregion
+        */
+        if(HP >= 70)       print("安全");
+        else if(HP >= 50)  print("警告");
+        else if(HP >= 20)  print("危險");
+        else if(HP > 0)    print("快死了");
+        else               print("死了");
+    }
+}
